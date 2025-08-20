10ab9146ceaa8c09b9b8982ff0b3b5f15fdf9093
B <B@DESKTOP-L7M5D7L>
2025-08-19 22:57:30 -0400

    Extracted out taglist for telemetry

---
+++ SharedLibrary/TelemetryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace SharedLibrary.Extensions;
+
+public static class TelemetryExtensions
+{
+    public static TagList BuildMetricTags(Guid importId, string sourceSystem, string triggeredBy)
+    {
+        return new TagList
+        {
+            { "import_id", importId.ToString() },
+            { "source_system", sourceSystem },
+            { "triggered_by", triggeredBy }
+        };
+    }
+}
