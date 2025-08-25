b3e9d6117304a3fea5bb26d9961e836002ff9149
B <B@DESKTOP-L7M5D7L>
2025-08-25 11:55:56 -0400

    Turned SharedLibrary into a package on my GitHub

---
+++ IngestionService/src/IngestionService/Infrastructure/Telemetry/TelemetryConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+
+public static class TelemetryConfigurationExtensions
+{
+    public static void AddCustomTelemetry(this IServiceCollection services, string[] meterNames)
+    {
+        services.AddOpenTelemetry()
+            .WithMetrics(metrics =>
+            {
+                metrics.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("IngestionService"));
+
+                foreach (var meter in meterNames)
+                {
+                    metrics.AddMeter(meter);
+                }
+
+                metrics.AddRuntimeInstrumentation();
+                metrics.AddPrometheusExporter(options =>
+                {
+                    options.ScrapeResponseCacheDurationMilliseconds = 0;
+                });
+            });
+    }
+}
