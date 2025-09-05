6eedbbd6fe8e5a20b1ecc3fa89f1e2ab68c49a66
B <B@DESKTOP-L7M5D7L>
2025-09-04 22:04:19 -0400

    Added partial class ComicCsvRecordDto and record ComicCsvRecord for common use. (AB#6)

---
+++ SharedLibrary/ComicCsvRecordDto.cs
@@ -0,0 +1,8 @@
+﻿using Facet;
+
+[Facet(typeof(ComicCsvRecord))]
+public partial class ComicCsvRecordDto
+{
+    public decimal? Value { get; set; }
+    public string? CoverArtPath { get; set; }
+}
