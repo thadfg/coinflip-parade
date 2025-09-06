a74c50272c791a42df00a0791022c5d9eae73c03
B <B@DESKTOP-L7M5D7L>
2025-09-05 23:41:00 -0400

    Defined schema for Domain Entity (1 completed from User Story) Created docker-compose.local in the service root to spin up a PostgreSql container.  (AB#11)

---
+++ PersistenceService/src/PersistenceService/Domain/Entities/EventEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersistenceService.Domain.Entities;
+
+[Table("events")]
+public class EventEntity
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("aggregate_id")]
+    public Guid AggregateId { get; set; }
+
+    [Column("event_type")]
+    public string EventType { get; set; } = default!;
+
+    [Column("event_data", TypeName = "jsonb")]
+    public string EventData { get; set; } = default!;
+
+    [Column("occurred_at")]
+    public DateTimeOffset OccurredAt { get; set; }
+}
---
+++ PersistenceService/src/PersistenceService/Infrastructure/EventDbContext .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersistenceService.Domain.Entities;
+using System.Collections.Generic;
+
+namespace PersistenceService.Infrastructure;
+
+public class EventDbContext : DbContext
+{
+    public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) { }
+
+    public DbSet<EventEntity> Events => Set<EventEntity>();
+}
