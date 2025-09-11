48b9f109f4f6217596b64050f00ffdacdf8a1976
B <B@DESKTOP-L7M5D7L>
2025-09-10 23:09:58 -0400

    Added 2 classes that will be used accross multiple services

---
+++ SharedLibrary/DeadLetterEnvelope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SharedLibrary.Models;
+
+public class DeadLetterEnvelope<T>
+{
+    public string ImportId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Reason { get; set; }
+    public T FailedPayload { get; set; }
+}
