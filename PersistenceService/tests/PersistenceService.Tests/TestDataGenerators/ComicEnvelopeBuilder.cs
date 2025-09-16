e584c41e6c37e4c7c43e8962fa648d5562afd05c
B <B@DESKTOP-L7M5D7L>
2025-09-16 00:45:35 -0400

    Integration Test added.  Was not pleasant.  Maybe there is an easier way to do this. (AB#13)

---
+++ PersistenceService/tests/PersistenceService.Tests/TestDataGenerators/ComicEnvelopeBuilder.cs
@@ -0,0 +1,31 @@
+﻿using SharedLibrary.Facet;
+using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenceService.Tests.TestDataGenerators;
+
+public static class ComicEnvelopeBuilder
+{
+    public static KafkaEnvelope<ComicCsvRecordDto> Build(Guid importId)
+    {
+        return new KafkaEnvelope<ComicCsvRecordDto>
+        {
+            ImportId = importId.ToString(),
+            Payload = new ComicCsvRecordDto
+            {
+                PublisherName = "Dark Horse",
+                SeriesName = "Hellboy",
+                FullTitle = "Hellboy: Seed of Destruction",
+                ReleaseDate = "1994-03-01",
+                InCollection = "Yes",
+                Value = (decimal)12.99,
+                CoverArtPath = "/covers/hellboy1.jpg"
+            }
+        };
+    }
+}
+
