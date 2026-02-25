53c095b7308dd536d7e386191a7c255acc02d04c
bb <thadfg@gmail.com>
2026-02-25 09:19:14 -0500

    AB#39 Update metrics expressions in order to correct the bug of metrics not showing for ingestion service.  This one correct ingestion - uptime panel.

--- IngestionService/src/IngestionService/Infrastructure/Telemetry/TelemetryConfigurationExtensions.cs
+++ IngestionService/src/IngestionService/Infrastructure/Telemetry/TelemetryConfigurationExtensions.cs
@@ -39,6 +39,7 @@ public static class TelemetryConfigurationExtensions
                     .AddAspNetCoreInstrumentation()        
                     .AddHttpClientInstrumentation()            
                     .AddMeter(meterNames)
+                    .AddMeter("ComicIngestion.Meter")
                     .AddPrometheusExporter()
                     .AddOtlpExporter();
 
