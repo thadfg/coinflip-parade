5167d61782508213bba728b3c0f8ae291b19c7d6
thadfg <thadfg@gmail.com>
2026-03-30 20:38:55 -0400

    Change namespace from PersistenceService to SharedLibrary
    AB#43 Namespace needed to be updated

--- SharedLibrary/ComicRecordEntity.cs
+++ SharedLibrary/ComicRecordEntity.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PersistenceService.Domain.Entities;
+namespace SharedLibrary.Models
 
 [Table("comiccollection")]
 [Index(nameof(ReleaseDate))]
