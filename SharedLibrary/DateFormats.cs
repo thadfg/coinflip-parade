8a92b5247f1d62f139e02128dc8faac978136ee8
B <B@DESKTOP-L7M5D7L>
2026-04-23 09:19:40 -0400

    AB#43 Updated DateFormats to accomodate additional found date formats.

--- SharedLibrary/DateFormats.cs
+++ SharedLibrary/DateFormats.cs
@@ -2,8 +2,8 @@ namespace SharedLibrary.Constants
 {
     public static class DateFormats
     {
-        public static readonly string[] AcceptedFormats = { "yyyy-MM-dd", "MM/dd/yyyy", "M/d/yyyy", "dd-MMM-yy", "d-MMM-yy" };
+        public static readonly string[] AcceptedFormats = { "yyyy-MM-dd", "MM/dd/yyyy", "M/d/yyyy", "dd-MMM-yy", "d-MMM-yy", "MMM d, yyyy" };
         
-        public const string DisplayFormats = "YYYY-MM-DD, MM/DD/YYYY, M/D/YYYY, or D-MMM-YY";
+        public const string DisplayFormats = "YYYY-MM-DD, MM/DD/YYYY, M/D/YYYY, D-MMM-YY, or MMM DD, YYYY";
     }
 }
